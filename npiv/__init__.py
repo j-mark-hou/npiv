0a35047166197294702b680c7352df88723d1110
j. mark hou <mark@jmhou.com>
2018-10-08 23:49:34 -0400

    added stage1 training for NonparametricIV, added to __init__.py

--- npiv/__init__.py
+++ npiv/__init__.py
@@ -1,3 +1,4 @@
 from . import iv_simulator
 from . import model_wrapper
 from . import custom_objectives
+from .nonparametric_iv import NonparametricIV
--- npiv/nonparametric_iv.py
+++ npiv/nonparametric_iv.py
@@ -7,13 +7,12 @@ class NonparametricIV:
     '''
     class encapsulating the entire nonparametric instrumental variables process
     '''
-    def __init__(self, df:pd.DataFrame, exog_x_cols:list, endog_x_col:str, y_col:str, 
-                    stage1_data_frac:float=.7, stage1_train_frac:float=.7,
-                    numpy_random_seed:int=0,
+    def __init__(self, df:pd.DataFrame, exog_x_cols:list, instrument_cols:list, endog_x_col:str, y_col:str, 
+                    stage1_data_frac:float=.5, stage1_train_frac:float=.7, stage2_train_frac:float=.7,
                     stage1_params:dict=None, stage1_models:dict=None,
                     stage2_model_type:str='lgb', stage2_params:dict=None):
         '''
-        df : the dataframe with training data
+        df : the dataframe with training data.  each row should correspond to a single observation.
         exog_x_cols, endog_x_col, y_col : exogenous features, endogenous feature (singular), and target variable,
                                           must be columns in df
         stage1_data_frac: how much of the data to use to train stage1 vs stage2 (they're trained on separate sets)
@@ -34,9 +33,87 @@ class NonparametricIV:
         # init stage2 parameters
         self._init_stage2(stage2_model_type, stage2_params)
         # create the dataframe required
-        self._init_data(df, exog_x_cols, endog_x_col, y_col, stage1_data_frac, stage1_train_frac)
+        self._init_data(df, exog_x_cols, instrument_cols, endog_x_col, y_col, stage1_data_frac, stage1_train_frac, stage2_train_frac)
 
 
+    def train_stage1(self, force=False, print_fnc=print):
+        '''
+        trains stage1 models, which predict quantiles, given the input parameters stored in self.stage1_params
+        force : force training even if we've already trained
+        print_fnc : some function for printing/logging.
+        '''
+        if not self._train_stage1_enabled:
+            raise ValueError("training stage1 is not enabled, as stage1 models were directly input during initialization")
+        try:
+            self.stage1_models
+            if not force:
+                raise ValueError("stage1 models already exist, set force=True to force retraining")
+        except AttributeError:
+            pass
+
+        # lgb datasets for training.  predict endogenous x as a function of exogenous x
+        x_cols = self.exog_x_cols + self.instrument_cols
+        y_col = self.endog_x_col
+        df_train = self.data.loc[self.data['_purpose_']=='train1',:]
+        df_val = self.data.loc[self.data['_purpose_']=='val1',:]
+        dat_train = lgb.Dataset(df_train[x_cols], label=df_train[y_col])
+        dat_val = lgb.Dataset(df_val[x_cols], label=df_val[y_col])
+        # ok, now start training
+        models = {}
+        for alpha, params in self.stage1_params.items():
+            print_fnc("alpha={}".format(alpha))
+            print_every = params['num_iterations']//10 # print stuff 20 times
+            eval_results={} # store evaluation results as well with the trained model
+            # copy the params because lgb modifies it during run...?
+            gbm = lgb.train(params.copy(), train_set=dat_train, 
+                            valid_sets=[dat_train, dat_val], valid_names=['train', 'val'],
+                            verbose_eval=print_every,
+                            callbacks=[lgb.record_evaluation(eval_results)]
+                            )
+            gbm.eval_results = eval_results
+            models[alpha] = gbm
+        # save the trained models
+        self.stage1_models = models
+
+    def train_stage2(self, force=False, print_fnc=print):
+        '''
+        trains stage2 models, which takes quantiles and tries to predict
+        self.y_col as a function of self.exog_x_col and a synthetic version of
+        self.endog_x_col generated via self.stage1_models
+
+        '''
+        try:
+            self.stage2_model
+            if not force:
+                raise ValueError("stage2 model already trained, set force=True to force retraining")
+        except AttributeError:
+            pass
+
+        # lgb datasets for training.  predict endogenous x as a function of exogenous x
+        df_train = self.data.loc[self.data['_purpose_']=='train1',:]
+        df_val = self.data.loc[self.data['_purpose_']=='val1',:]
+        dat_train = lgb.Dataset(df_train[self.exog_x_cols], label=df_train[self.endog_x_col])
+        dat_val = lgb.Dataset(df_val[self.exog_x_cols], label=df_val[self.endog_x_col])
+        # ok, now start training
+        models = {}
+        for alpha, params in self.stage1_params.items():
+            print_fnc("alpha={}".format(alpha))
+            print_every = params['num_iterations']//10 # print stuff 20 times
+            eval_results={} # store evaluation results as well with the trained model
+            # copy the params because lgb modifies it during run...?
+            gbm = lgb.train(params.copy(), train_set=dat_train, 
+                            valid_sets=[dat_train, dat_val], valid_names=['train', 'val'],
+                            verbose_eval=print_every,
+                            callbacks=[lgb.record_evaluation(eval_results)]
+                            )
+            gbm.eval_results = eval_results
+            models[alpha] = gbm
+        # save the trained models
+        self.stage1_models = models
+
+
+
+    ### helper methods
     def _init_stage1(self, stage1_models:dict, stage1_params:dict):
         '''
         initialization for the stage1 models/params
@@ -66,6 +143,7 @@ class NonparametricIV:
                                                 'objective': 'quantile',
                                                 'alpha': alpha, 
                                                 'metric': 'quantile',
+                                                'num_iterations':1000,
                                                 'num_leaves': 5,
                                                 'learning_rate': 0.01,
                                                 'feature_fraction': 0.5,
@@ -76,7 +154,7 @@ class NonparametricIV:
                                                 }
                                         for alpha in qtls}
 
-    def _init_stage2(stage2_model_type:str, stage2_params:dict):
+    def _init_stage2(self, stage2_model_type:str, stage2_params:dict):
         '''
         parameters required for the second stage model
         '''
@@ -91,30 +169,32 @@ class NonparametricIV:
                 self.stage2_params = params
             else:
                 params = {
-                    'num_threads':4,
-                    'objective': None,
-                    'metric': None,
-                    'num_leaves': 5,
-                    'learning_rate': 0.2,
-                    'feature_fraction': 0.5,
-                    'bagging_fraction': 0.8,
-                    'bagging_freq': 5,
-                    'max_delta_step':.1,
-                    'min_gain_to_split':10,
-                }
+                            'num_threads':4,
+                            'objective': None,
+                            'metric': None,
+                            'num_leaves': 5,
+                            'learning_rate': 0.2,
+                            'feature_fraction': 0.5,
+                            'bagging_fraction': 0.8,
+                            'bagging_freq': 5,
+                            'max_delta_step':.1,
+                            'min_gain_to_split':10,
+                            }
                 self.stage2_params = params
         elif stage2_model_type == 'linear':
             self.stage2_params = None # no params needed if linear objective
 
-    def _init_data(self, df:pd.DataFrame, exog_x_cols:list, endog_x_col:str, y_col:str, 
-                    stage1_data_frac:float, stage1_train_frac:float, numpy_random_seed:int):
+    def _init_data(self, df:pd.DataFrame, exog_x_cols:list, instrument_cols:list, endog_x_col:str, y_col:str, 
+                    stage1_data_frac:float, stage1_train_frac:float, stage2_train_frac:float):
         '''
         initialization required to create the training data
         '''
         # set the various columns
-        if set(list(exog_x_cols) + [endog_x_col, y_col]).difference(df.columns):
+        keep_cols = exog_x_cols + instrument_cols + [endog_x_col, y_col]
+        if set(keep_cols).difference(df.columns):
             raise ValueError("exog_x_cols, endog_x_col, y_col must all be columns of df")
-        self.exog_x_cols = list(exog_x_cols)
+        self.exog_x_cols = exog_x_cols
+        self.instrument_cols = instrument_cols
         self.endog_x_col = endog_x_col
         self.y_col = y_col
         # set the fraction of data to use for stage1-training
@@ -122,8 +202,24 @@ class NonparametricIV:
             raise ValueError("stage1_data_frac, stage1_train_frac must both be in (0,1)")
         self.stage1_data_frac = stage1_data_frac
         self.stage1_train_frac = stage1_train_frac
-        # copy the data
-        df = df[[self.exog_x_cols + [self.endog_x_col, self.y_col]]].copy()
+        # copy just the columns we need
+        df = df[keep_cols].copy()
+        df['id'] = np.arange(df.shape[0])
         # generate an indicator for what each observation in the data will be used for
-        uniform_random = np.random.uniform(size=)
-        df['_purpose_'] = 
+        # generate groups for training/validation
+        # the first stage1_data_frac of the observations will be used for stage1,
+        #  of which the first stage1_train_frac will be train and the rest as validation set.
+        # of the stage2 data, the first stage2_train_frac will be used for training, rest 
+        #  as validation set.
+        n = df.shape[0]
+        stage1_train_cutoff = int(n * stage1_data_frac * stage1_train_frac)
+        stage1_val_cutoff = int(n * stage1_data_frac)
+        stage2_train_cutoff = int(n * stage1_data_frac + (1 - stage1_data_frac) * stage2_train_frac )
+        df['_purpose_'] = None
+        df.loc[df['id']<stage1_train_cutoff,                                 '_purpose_'] = 'train1'
+        df.loc[(df['id']>=stage1_train_cutoff)&(df['id']<stage1_val_cutoff), '_purpose_'] = 'val1'
+        df.loc[(df['id']>=stage1_val_cutoff)&(df['id']<stage2_train_cutoff), '_purpose_'] = 'train2'
+        df.loc[(df['id']>=stage2_train_cutoff)                             , '_purpose_'] = 'val2'
+        # save the data
+        self.data = df
+

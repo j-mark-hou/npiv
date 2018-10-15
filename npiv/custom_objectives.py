66fe693d5d7a07c1578888d2f763bdf103466163
j. mark hou <mark@jmhou.com>
2018-10-15 00:15:36 -0400

    docstring and type hints for npiv/custom_objectives.py

--- npiv/custom_objectives.py
+++ npiv/custom_objectives.py
@@ -2,34 +2,34 @@ import pandas as pd
 import numpy as np
 
 
-def grouped_sse_loss(yhat, y, grps):
+def grouped_sse_loss(yhat:np.ndarray, y:np.ndarray, grps:np.ndarray) -> float:
     '''
     grouped sum-of-squared-errors loss function.
-    Inputs:
-        - yhat : n-length real array
-        - y :  n-length real array
-        - grps: n-length integer array indicating the group of each observation
+    Args:
+        yhat : n-length real array
+        y :  n-length real array
+        grps: n-length integer array indicating the group of each observation
     Returns:
-        - a positive number equal to the summed squared loss
+        a positive number equal to the summed squared loss
     '''
     df = pd.DataFrame({'yhat':yhat, 'y':y, 'grp':grps})
     df_grp = df.groupby('grp')[['yhat', 'y']].sum()
     loss =np.sum(np.square(df_grp['yhat']-df_grp['y']))
     return loss
 
-def grouped_sse_loss_linear(coefs, df, x_cols, y_col, grp_col):
+def grouped_sse_loss_linear(coefs:np.ndarray, df:pd.DataFrame, 
+                            x_cols:list, y_col:str, grp_col:str) -> float:
     '''
-    computes yhat via  linear model, then computes the grouped sse
-    via grouped_sse_loss above.
-    Inputs:
-        - coefs : a real-valued array of coefficients, where
-                    coefs[0] is the interceptt and coefs[1], coefs[2], ...
-                    correspond to x_cols[0], x_cols[1],...
-        - df : a dataframe with all the required data
-        - x_cols : columns of df corresponding to the entries in coef, in 
-                    exactly that order
-        - y_col : the true y-values
-        - grp_col : the column with grouping information
+    computes predicted y-hat via a linear model, then calls grouped_sse_loss above.
+    Args:
+        coefs: a real-valued array of coefficients, where
+                  coefs[0] is the interceptt and coefs[1], coefs[2], ...
+                  correspond to x_cols[0], x_cols[1],...
+        df: a dataframe with all the required data
+        x_cols: columns of df corresponding to the entries in coef, in 
+                  exactly that order
+        y_col: the true y-values
+        grp_col: the column with grouping information
     '''
     _coefs = coefs[1:]
     yhat = df[x_cols].multiply(_coefs).sum(axis=1) + coefs[0]
@@ -37,17 +37,20 @@ def grouped_sse_loss_linear(coefs, df, x_cols, y_col, grp_col):
     return loss
 
 
-def grouped_sse_loss_grad_hess(yhat, y, grps):
+def grouped_sse_loss_grad_hess(yhat:np.ndarray, y:np.ndarray, grps:np.ndarray):
     '''
     produces first and second derivatives of the grouped_sse_loss
     with respect to each of the entries in yhat.
-    Inputs: grouped_sse_loss
+    Inputs: 
+        yhat : n-length real array
+        y :  n-length real array
+        grps: n-length integer array indicating the group of each observation
     Output:
-        - two vectors, each of the same length as the inputs
-          the first one being the first derivatives of the loss
-          with respect to each value of yhat, and the second
-          being the corresponding second derivatives.  the expressions
-          can be trivially computed.
+        two vectors, each of the same length as the inputs
+        the first one being the first derivatives of the loss
+        with respect to each value of yhat, and the second
+        being the corresponding second derivatives.  the expressions
+        can be trivially computed.
     '''
     df = pd.DataFrame({'yhat':yhat, 'y':y, 'grp':grps})
     df_grp = df.groupby('grp')[['yhat', 'y']].sum()
